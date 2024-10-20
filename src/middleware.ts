5e30dddc73ae2a5654b5dab07d1e4698595cb859
Michael Novotny <manovotny@gmail.com>
2024-10-19 21:16:16 -0500

    Adds middleware

---
+++ src/middleware.ts
@@ -0,0 +1,21 @@
+export function onRequest(context, next) {
+  if (context.url.pathname.endsWith("2-challenge")) {
+    return context.rewrite(
+      new URL(
+        "/1-nextjs-conf/1-community-challenges/1-challenge",
+        context.request.url
+      )
+    );
+  }
+
+  if (context.url.pathname.endsWith("3-challenge")) {
+    return context.rewrite(
+      new URL(
+        "/1-nextjs-conf/1-community-challenges/2-challenge",
+        context.request.url
+      )
+    );
+  }
+
+  return next();
+}
