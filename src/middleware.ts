16f2a81f24d0e3d1bd1740f0b5cbfc599e2242ed
Michael Novotny <manovotny@gmail.com>
2024-10-22 16:34:45 -0700

    Comment out middleware for now

--- src/middleware.ts
+++ src/middleware.ts
@@ -4,23 +4,23 @@ export function onRequest(
   context: AstroGlobal,
   next: () => Promise<Response>
 ): Promise<Response> {
-  if (context.url.pathname.endsWith("2-challenge")) {
-    return context.rewrite(
-      new URL(
-        "/1-nextjs-conf/1-community-challenges/1-challenge",
-        context.request.url
-      )
-    );
-  }
+  // if (context.url.pathname.endsWith("2-challenge")) {
+  //   return context.rewrite(
+  //     new URL(
+  //       "/1-nextjs-conf/1-community-challenges/1-challenge",
+  //       context.request.url
+  //     )
+  //   );
+  // }
 
-  if (context.url.pathname.endsWith("3-challenge")) {
-    return context.rewrite(
-      new URL(
-        "/1-nextjs-conf/1-community-challenges/2-challenge",
-        context.request.url
-      )
-    );
-  }
+  // if (context.url.pathname.endsWith("3-challenge")) {
+  //   return context.rewrite(
+  //     new URL(
+  //       "/1-nextjs-conf/1-community-challenges/2-challenge",
+  //       context.request.url
+  //     )
+  //   );
+  // }
 
   return next();
 }
