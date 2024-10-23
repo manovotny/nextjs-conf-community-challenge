3d740a639c2b2ad9f2d709ae6fc55d6c23ad3a91
Michael Novotny <manovotny@gmail.com>
2024-10-23 16:42:46 -0700

    Enlarge image

---
+++ src/components/EnlargeImage.tsx
@@ -0,0 +1,25 @@
+export default function EnlargeImage({ src }) {
+  function handleClick() {
+    var background = document.createElement("div");
+    background.className =
+      "flex items-center justify-center bg-black bg-opacity-50 w-full h-screen absolute top-0 left-0 right-0 bottom-0 cursor-pointer z-[500] p-5";
+    background.addEventListener("click", function () {
+      document.body.removeChild(background);
+    });
+
+    var img = document.createElement("img");
+    img.src = src;
+    img.className = "border border-gray-200 dark:border-gray-800 rounded-lg";
+    background.appendChild(img);
+
+    document.body.appendChild(background);
+  }
+
+  return (
+    <img
+      className="border border-gray-200 dark:border-gray-800 rounded-lg cursor-pointer"
+      src={src}
+      onClick={handleClick}
+    />
+  );
+}
