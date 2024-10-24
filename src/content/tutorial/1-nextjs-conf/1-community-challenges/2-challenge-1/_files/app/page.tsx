3ba1f3c82718b8772b9add7010bc9b1916d6131c
Michael Novotny <manovotny@gmail.com>
2024-10-24 10:49:45 -0700

    challenge 1

---
+++ src/content/tutorial/1-nextjs-conf/1-community-challenges/2-challenge-1/_files/app/page.tsx
@@ -0,0 +1,82 @@
+const cardData = [
+  {
+    title: "Total revenue",
+    value: "$45,321.89",
+    changeValue: "+20.1% from last month",
+  },
+  {
+    title: "Subscriptions",
+    value: "+2350",
+    changeValue: "+180.1% from last month",
+  },
+  {
+    title: "Sales",
+    value: "+12,234",
+    changeValue: "+19% from last month",
+  },
+  {
+    title: "Active now",
+    value: "+537",
+    changeValue: "+201 since last hour",
+  },
+];
+const tableData = [
+  {
+    orderNumber: "1039",
+    customer: "Garry Crona",
+    status: "Ordered",
+    actions: ["Show details", "Cancel"],
+  },
+  {
+    orderNumber: "1040",
+    customer: "Ramona Jenkins",
+    status: "Shipped",
+    actions: ["Show details"],
+  },
+  {
+    orderNumber: "1041",
+    customer: "Bernadette Cole",
+    status: "Shipped",
+    actions: ["Show details"],
+  },
+  {
+    orderNumber: "1042",
+    customer: "Allison Ziemann I",
+    status: "Ordered",
+    actions: ["Show details", "Cancel"],
+  },
+  {
+    orderNumber: "1043",
+    customer: "Harold Purdy",
+    status: "Delivered",
+    actions: ["Show details"],
+  },
+  {
+    orderNumber: "1044",
+    customer: "Krista Beahan",
+    status: "Shipped",
+    actions: ["Show details"],
+  },
+  {
+    orderNumber: "1045",
+    customer: "Ann Romaguera V",
+    status: "Delivered",
+    actions: ["Show details"],
+  },
+  {
+    orderNumber: "1046",
+    customer: "Tami Kessler Sr.",
+    status: "Ordered",
+    actions: ["Show details", "Cancel"],
+  },
+  {
+    orderNumber: "1047",
+    customer: "Miss Glen Feil",
+    status: "Shipped",
+    actions: ["Show details"],
+  },
+];
+
+export default function Page() {
+  return <p>TODO</p>;
+}
