ac3f5db21174b85ef04b4274bb17feb3b674f44f
Michael Novotny <manovotny@gmail.com>
2024-10-23 20:38:42 -0700

    Moves assets to consts

--- src/content/tutorial/1-nextjs-conf/1-community-challenges/3-challenge-2/_files/app/page.tsx
+++ src/content/tutorial/1-nextjs-conf/1-community-challenges/3-challenge-2/_files/app/page.tsx
@@ -1,3 +1,11 @@
+// Use in img src
+const changeThemeIcon = "/change-theme-icon.svg";
+const copyCurrentUrlIcon = "/copy-current-url-icon.svg";
+const createNewIcon = "/create-new-icon.svg";
+const searchProjectsIcon = "/search-projects-icon.svg";
+const searchTeamsIcon = "/search-teams-icon.svg";
+const shiftArrowIcon = "/shift-arrow-icon.svg";
+
 export default function Page() {
   return <p>TODO</p>;
 }
--- src/content/tutorial/1-nextjs-conf/1-community-challenges/4-challenge-3/_files/app/page.tsx
+++ src/content/tutorial/1-nextjs-conf/1-community-challenges/4-challenge-3/_files/app/page.tsx
@@ -1,3 +1,12 @@
+const boldText = "The React Framework for the Web.";
+const regularText =
+  "Used by some of the world's largest companies, Next.js enables you to create high-quality web applications with the power of React components.";
+
+// Use in img src
+const nextjsConfHero = "/nextjs-conf-hero.svg";
+const nextjsConfLogo = "/nextjs-conf-logo.svg";
+const neztjsLogo = "/nextjs-logo.svg";
+
 export default function Page() {
   return <p>TODO</p>;
 }
